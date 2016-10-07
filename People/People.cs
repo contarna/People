700c8b2a1c3ad5f11beb05e4c12d7736d0e9fa44
contarna <lipengsong@hotmail.com>
2016-10-08 00:01:46 +0800

    添加项目文件。

---
+++ People/People.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace People
+{
+    class People :DictionaryBase
+    {
+        public void Add(string newName, Person newPerson)
+        {
+            Dictionary.Add(newName, newPerson);
+        }
+
+        public void Remove(string newName)
+        {
+            Dictionary.Remove(newName);
+        }
+
+        public Person this[string newName]
+        {
+            get { return (Person)Dictionary[newName]; }
+            set { Dictionary[newName] = value; }
+        }
+    }
+}
